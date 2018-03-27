c6fa6409294210108cd7a5afe6fd4f2825f80c83
dmuxgit <37579404+dmuxgit@users.noreply.github.com>
2018-03-27 19:32:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,15 +19,14 @@ export default class App extends Component {
       selectedEntity: '',
       entityList: []
     };
-    this.data = [];
     this.regionsArr = [];
     this.regionsSet = '';
     this.regionsUniqArr = [];
+    this.toRenderEntityArr = [];
+    this.selcRegionValue = '';
     this.getRegionsList = this.getRegionsList.bind(this);
-    this.getRegionsEntityList = this.getRegionsEntityList.bind(this);
+    this.getRegionVal = this.getRegionVal.bind(this);
   }
-
-
 
   componentWillMount() {
     /*axios.get('http://localhost:3000/assets/plandata.json')
@@ -41,11 +40,9 @@ export default class App extends Component {
   type: "GET",
   dataType: "json"
 });
-
 request.done(function(response) {
   console.log('success');
 });
-
 request.fail(function(jqXHR, textStatus) {
   console.log('error occurred ' + textStatus);
 });*/
@@ -65,9 +62,9 @@ request.fail(function(jqXHR, textStatus) {
       return regionsArr;
     });
     this.regionsSet = new Set(regionsArr);
-    regionsArr.map(val => console.log(val));
-    console.log("after Set");
-    console.log(this.regionsSet);
+    //regionsArr.map(val => console.log(val));
+    //console.log("after Set");
+    //console.log(this.regionsSet);
     this.regionsSet.forEach(element => {
       this.regionsUniqArr.push(element);
       return this.regionsUniqArr;
@@ -78,9 +75,66 @@ request.fail(function(jqXHR, textStatus) {
     this.setState({selectedEntity: entityName});
   }
 
-  getRegionsEntityList(regionVal) {
-    var regionValue = regionVal;
-    console.log(regionValue + " test");
+  getRegionVal(regionVal) {
+    this.selcRegionValue = regionVal;
+    //console.log(this.selcRegionValue + " test");
+
+    switch (this.selcRegionValue) {
+      case 'America':
+        this.toRenderEntityArr.length = 0;
+        data.plan.map(element => {
+          if(element.region === 'America') {
+            this.toRenderEntityArr.push(element.entity);
+          }
+          return this.toRenderEntityArr;
+        });
+        this.toRenderEntityArr.map(val => console.log(val));
+        break;
+        case 'Africa':
+          this.toRenderEntityArr.length = 0;
+          data.plan.map(element => {
+            if(element.region === 'Africa') {
+              this.toRenderEntityArr.push(element.entity);
+            }
+            return this.toRenderEntityArr;
+          });
+          this.toRenderEntityArr.map(val => console.log(val));
+          break;
+        case 'Asia':
+          this.toRenderEntityArr.length = 0;
+          data.plan.map(element => {
+            if(element.region === 'Asia') {
+              this.toRenderEntityArr.push(element.entity);
+            }
+            return this.toRenderEntityArr;
+          });
+          this.toRenderEntityArr.map(val => console.log(val));
+          break;
+        case 'Europe':
+          this.toRenderEntityArr.length = 0;
+          data.plan.map(element => {
+            if(element.region === 'Europe') {
+              this.toRenderEntityArr.push(element.entity);
+            }
+            return this.toRenderEntityArr;
+          });
+          this.toRenderEntityArr.map(val => console.log(val));
+          break;
+        case 'Oceania':
+          this.toRenderEntityArr.length = 0;
+          data.plan.map(element => {
+            if(element.region === 'Oceania') {
+              this.toRenderEntityArr.push(element.entity);
+            }
+            return this.toRenderEntityArr;
+          });
+          this.toRenderEntityArr.map(val => console.log(val));
+          break;
+        default:
+          console.log('default');
+    }
+    console.log(this.toRenderEntityArr);
+    return this.toRenderEntityArr;
   }
 
   getRegionsList(data) {
@@ -102,17 +156,14 @@ request.fail(function(jqXHR, textStatus) {
         </header>
         <main className="content-wrapper">
           <div className="row">
-            <RegionsList regionsUniqArr={this.regionsUniqArr} getRegionsEntityList={this.getRegionsEntityList}/>
+            <RegionsList regionsUniqArr={this.regionsUniqArr} getRegionVal={this.getRegionVal}/>
             <p className="selcEntity">{this.state.selectedEntity}</p>
           </div>
           <div className="row">
             <div className="col-sm-2 entityList">
-              <EntityList onEntitySelection={this.handleSelectedEntity}/>
+              <EntityList toRenderEntityArr={this.toRenderEntityArr} onEntitySelection={this.handleSelectedEntity}/>
             </div>
             <div className="col-sm-7 center"><p>Column 2</p>
-              <ul>
-
-              </ul>
             </div>
             <aside className="col-sm-3"><p>Column 3</p></aside>
           </div>
