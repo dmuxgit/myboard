1708e2806e97bd86f941f8a2df98d89ec3709f45
dmuxgit <37579404+dmuxgit@users.noreply.github.com>
2018-04-03 13:45:19 +0530

    Update entitylist.js

--- src/components/entitylist.js
+++ src/components/entitylist.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 export default class EntityList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      entityArr:  this.props.toRenderEntityArr
+    };
 
     this.selcEntityName = '';
-    this.entityArr = this.props.toRenderEntityArr;
+    //this.entityArr = this.props.toRenderEntityArr;
     this.getEntityName = this.getEntityName.bind(this);
   }
 
@@ -14,8 +17,23 @@ export default class EntityList extends Component {
     this.props.onEntitySelection(this.selcEntityName);
   }
 
+  componentWillMount() {
+    window.addEventListener('load', this.test());
+  }
+  /*componentWillReceiveProps(nextProps) {
+    this.setState({
+      entityArr: nextProps.toRenderEntityArr
+    });
+    //window.addEventListener('load', this.test());
+    console.log(this.state.entityArr);
+  }*/
+
+  test() {
+    console.log(this.state.entityArr);
+  }
+
   render() {
-    var renderEntities = this.entityArr.map((entity, index) => {
+    var renderEntities = this.state.entityArr.map((entity, index) => {
       return <button key={index} type="button" className="btn btn-success" onClick={this.getEntityName}> { entity } </button>
     });
     return (
